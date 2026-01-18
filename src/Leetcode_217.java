c4c1b9cc263a8e5900567ea45d51b3c1737e150c
NarieWynn <huynhngocnguyen24@gmail.com>
2026-01-18 13:43:59 +0700

    Leetcode 217- O(n)- Easy

---
+++ src/Leetcode_217.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+public class Leetcode_217 {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map= new HashMap<>();
+        for(int i=0; i < nums.length; i++){
+            if(map.containsKey(nums[i])){
+                return true;
+            }
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return false;
+    }
+}
