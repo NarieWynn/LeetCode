8b78c0b0ba31afbef0dcdbff2100c79c2cf5191a
NarieWynn <huynhngocnguyen24@gmail.com>
2026-01-31 23:08:26 +0700

    20.Valid Parentheses- O(n) - Easy

---
+++ java/Leetcode_20.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Leetcode_20 {
+    public boolean isValid(String s) {
+        Stack<Character> stack= new Stack<>();
+        HashMap<Character, Character> map= new HashMap<>();
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+        for(int i=0; i < s.length(); i++){
+            char c = s.charAt(i);
+            if(map.containsKey(c)){
+                if(stack.isEmpty() || stack.pop() != map.get(c)){
+                    return false;
+                }
+        }
+        else{
+        stack.push(c);
+        }
+    }
+        return stack.isEmpty();
+    }
+}
