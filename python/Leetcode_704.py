2c2793292e1e24d28ab7a198a5462b17bb15ce93
NarieWynn <huynhngocnguyen24@gmail.com>
2026-02-02 21:58:45 +0700

    704.Binary Search - O(log n) -Easy

--- python/Leetcode_704.py
+++ python/Leetcode_704.py
@@ -4,10 +4,13 @@ class Solution:
         right= len(nums) - 1
         while left <= right:
             mid= left + (right - left)//2
+            
             if(nums[mid] == target):
                 return mid
+            
             elif(nums[mid] < target):
                 left= mid + 1
+
             elif(nums[mid] > target):
                 right= mid - 1
             
